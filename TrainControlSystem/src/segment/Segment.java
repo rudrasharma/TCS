bff1607fe01490b0f4f21ef30a7f30d773c00be0
rudrasharma <rudra.sharma@gmail.com>
2014-04-05 14:43:11 -0400

    override equals and hashcode for equals and hashcode for segment

--- TrainControlSystem/src/segment/Segment.java
+++ TrainControlSystem/src/segment/Segment.java
@@ -4,6 +4,7 @@ import core.Signal;
 
 public class Segment {
 	private int segmentId;
+
 	private Signal light;
 	private boolean holdingTrain;
 	
@@ -63,6 +64,33 @@ public class Segment {
 	 */
 	public void setSegmentId(int segmentId) {
 		this.segmentId = segmentId;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + segmentId;
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Segment other = (Segment) obj;
+		if (segmentId != other.segmentId)
+			return false;
+		return true;
 	}
 
 }
